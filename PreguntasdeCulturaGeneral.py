f6a4a97b97ce679599b897ea3c57688602a10ee6
Dariel Solorzano <dariel.solorzano@usil.pe>
2018-10-24 15:49:54 -0500

    Pregunta de cultura general por si se llega a borrar

---
+++ PreguntasdeCulturaGeneral.py
@@ -0,0 +1,28 @@
+'''PREGUNTAS DE CULTURA GENERAL'''
+
+'''
+1.	¿Quién escribió La Odisea?
+    A.	Mario Vargas Llosa
+    B.	Homero (R.C)
+    C.	Pablo Neruda
+2.	¿Con que instrumento se mide la temperatura?
+    A.	Odómetro
+    B.	Termómetro (R.C)
+    C.	Interferómetro
+3.	¿Cuándo empezó la II guerra mundial?
+    A.	1914(R.C)
+    B.	1915
+    C.	1913
+4.	¿Cuál es el metal más caro del mundo?
+    A.	Oro
+    B.	Diamante
+    C.	Rodio (R.C)
+5.	¿En qué país se encuentra la Universidad de Cambridge?
+    A.	España
+    B.	Reino Unido (R.C)
+    C.	Francia
+6.	¿Cuál es el libro sagrado del islam?
+    A.	Corán (R.C)
+    B.	Biblia
+    C.	Dhammapada
+'''
