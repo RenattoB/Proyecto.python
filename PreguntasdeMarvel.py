3a34ffae0cf0d3f70481aea6431f85fb44888a41
Dariel Solorzano <dariel.solorzano@usil.pe>
2018-10-24 15:47:01 -0500

    ListadePreguntasdeMarvel por si se borrara

---
+++ PreguntasdeMarvel.py
@@ -0,0 +1,33 @@
+                            '''PREGUNTAS DE MARVEL'''
+
+'''
+1- ¿Quién es el maestro que le enseña las artes místicas a Dr.Strange?
+    a-	Normamu
+    b-	Voldemort
+    c-	The Ancient (La respuesta correcta)
+
+2- ¿Quién cuida la gema? Y¿A quien sacrifica Thanos para obtenerla?
+    a-	Red Skull and Gamora
+    b-	Red Skull and minerva
+    c-	Gamora and Red Skull
+
+3- ¿En que lugar fue guardado la gema del espacio , tras los sucesos en el film Captain America?
+    a-	New York
+    b-	El Pentagono (Nave SHIELD)
+    c-	Asgard (respuesta correcta)
+
+4- ¿Dónde era guardada la gema del poder, en Guardians of the Galaxy?
+    a-	Aether
+    b-	Chitauri Scepter
+    c-	Orb (respuesta correcta)
+
+5- ¿Qué habilidades te otorga la gema de la realidad?
+    a-	Modifica las leyes de la realidad y Moldea las leyes de la física (respuesta correcta)
+    b-	Super velocidad y Viajar en el espacio-tiempo
+    c-	Traspasar cosas solidad e inmunidad a las balas
+
+6- ¿Quién le da la gema de la mente a visión (en los comics)?
+    a-	Iron man 
+    b-	Thor (respuesta correcta)
+    c-	Wanda
+'''
