071b36b438726607930a9095107986d089fb08a1
Dariel Solorzano <dariel.solorzano@usil.pe>
2018-10-29 09:03:12 -0600

    Preguntas de musica como respaldo

---
+++ PreguntasdeMusica.py
@@ -0,0 +1,33 @@
+'''             PREGUNTAS DE MUSICA          '''
+
+'''
+
+1- ¿ Que cantante era conocido como " The big O " ?
+    A- Des O'Connor 
+    B- Roy Orbison (RC)
+    C- Ozzy Osbourne
+
+2- ¿Cual de estos es el nombre de un álbum de los Beatles?
+    A- Lead Piping 
+    B- Candlestick
+    C- Revolver (RC)
+
+3- ¿Que banda de rock fundó Jimmy Page en 1968?
+    A- Black Sabbath
+    B- Manfred Mann
+    C- Led Zeppelin (RC)
+
+4- ¿En que año Culture Club obtuvo un numero 1 con Karma Chameleon?
+    A- 1983 (RC)
+    B- 1978
+    C- 1988
+
+5- ¿A cual de los siguientes grupos no pertenece Mike Shinoda?
+    A- Linkin Park
+    B- Fort Minor
+    C- X-Ecutioners (RC)
+
+6- ¿Cual fue el nombre de la pelicula de Eminem?
+    A- The White Rabbit
+    B- 8 mile (RC)
+    C- Stand
