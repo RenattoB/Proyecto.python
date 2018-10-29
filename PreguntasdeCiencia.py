b55013418718a8a67abfda0e7e05b54e8ade2445
Dariel Solorzano <dariel.solorzano@usil.pe>
2018-10-28 22:43:51 -0600

    Agregar preguntas de CIencia

---
+++ PreguntasdeCiencia.py
@@ -0,0 +1,37 @@
+'''                 PREGUNTAS DE QUIMICA Y FISICA              '''
+
+'''
+
+1- Cual de las 3 Leyes de Newton hace referencia la siguiente frase:
+   "El cambio de movimient es directamente proporcional a la fuerza motriz impresa y 
+    ocurre según la línea recta a lo largo de la cual aquella fuerza se imprime"
+    A- 1ra Ley de Newton
+    B- 2da Ley de Newton (RC)
+    C- 3ra Ley de Newton
+
+2- ¿Cual es el número atomico del Carbono?
+    A- 17
+    B- 14
+    C- 12 (RC)
+
+3- En un Proceso Isocóro se mantiene contante ........
+    A- La temperatura
+    B- EL numero de atomos
+    C- El volumen (RC)
+
+4- ¿Cual de los siguientes elementos no pertenece al grupo de los Gases Nobles?
+    A- Ne
+    B- Kr 
+    C- N (RC)
+
+5- ¿Cuando se considera un evento de Conservacion de Energia?
+    A- Cuando no existe la gravedad
+    B- Cuando no actuan fuerzas externas sobre el cuerpo (RC)
+    C- Si el cuerpo posee una masa despreciable
+
+6- ¿Cual es la relacion incorrecta entre elemento - grupo en la tabla periodica ?
+    A- C - Grupo 15 
+    B- Zn - Grupo 12 
+    C- Ni - Grupo 14 (RC)
+
+'''
