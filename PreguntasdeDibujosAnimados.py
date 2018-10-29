e356f29dae9fc481867bb643cf3c43bab76e6d9e
Dariel Solorzano <dariel.solorzano@usil.pe>
2018-10-29 10:52:50 -0600

    Preguntas de dibujos animados por respaldo

---
+++ PreguntasdeDibujosAnimados.py
@@ -0,0 +1,34 @@
+'''             PREGUNTAS SOBRE SERIES ANIMADAS         '''
+
+'''
+
+1- ¿Que tipo de relacion tiene Diego con Dora la exploradora?
+    A- Es su hermano
+    B- Es su primo (RC)
+    C- Es un amigo
+
+2- ¿Como se llama el futbolista que sufria del corazon en Los super campeones?
+    A- Mark Lenders
+    B- Philip Ross
+    C- Andy Johnson (RC)
+
+3- ¿Como se llamaban los mejores amigos de Jimmmy Neutro?
+    A- Sheen y Carl (RC)
+    B- Sheen y Carman
+    C- Carl y Shen
+
+4- ¿Como se llamaba la chica de la cual Timmy Turner estaba enamorado?
+    A- Tutty
+    B- Trixie (RC)
+    C- Wanda
+
+5- ¿Cual era el nombre del heroe que admiraba Bob Esponja?
+    A- Neptunoman
+    B- Chico Perseve
+    C- Sirenoman (RC)
+
+6- ¿En la serie Avatar cual fue el ultimo elemento que aprendio Aang?
+    A- Tierra
+    B- Agua
+    C- Fuego (RC)
+
