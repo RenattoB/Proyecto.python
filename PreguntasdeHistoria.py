945529ff3bb8ddcec4ed3fb1783de29343d46386
darielx123 <dariel-1000@hotmail.com>
2018-10-30 07:56:20 -0500

    Preguntas de historia (por si se borra)

---
+++ PreguntasdeHistoria.py
@@ -0,0 +1,28 @@
+'''              PREGUNTAS DE HISTORIA               '''
+
+'''
+
+1- Las teorías inmigracionistas que explican el poblamiento de América son la de la procedencia... (Alex Hdrlicka), la de origen... (Paul Rivet) y la de procedencia... (Mendes Correia).
+    A- australiana - asiatico - oceanico
+    B- asiatico - australiano - africana
+    C- asiatica - oceanico - australiano (RC)
+2- Las pinturas rupestres con escenas de caza, halladas en las paredes de las cuevas de Lascaux y Altamira, fueron realizadas con propósitos.
+    A- Magicos y Didacticos (RC)
+    B- Filosoficos y Economicos
+    C- Religiosos y Politicos
+3- Debido a los elevados gastos militares realizados por Inglaterra en la Guerra de los Siete Años contra Francia (1756-1763), el fisco quedó en ruina, motivo por el cual tuvo que adoptar medidas para generar ingresos. Una de estas fue...
+    A- El monopolio del Comercio de Azucar
+    B- El impuesto al consumo de alcohol (RC)
+    C- La separacion de los funcionarios criollos
+4- Luego de canceladas la mayoría de las encomiendas por la aplicación de las denominadas Nuevas Leyes (1542), la economía colonial estuvo basada en las rentas que proporcionaban ...
+    A- La mineria y la agricultura
+    B- La plata y el tributo indigena (RC)
+    C- La mita obrajera y la plata
+5- La segunda Revolución Industrial, caracterizada por el uso de nuevas fuentes de energía y la especialización de los procesos de trabajo se desarrolló a finales del siglo XIX en países como...
+    A- Francia y Belgica
+    B- Estados unidos y Alemania (RC)
+    C- Inglaterra y Holanda
+6- La ruptura de Augusto B. Leguía con el Partido Civil, hacia 1919, significó la culminación de un proceso económico iniciado a comienzos del siglo XX en el Perú. ¿Cuál fue su característica central?
+    A- El desplazamiento de la Hegemonia inglesa por la norteamericana (RC)
+    B- El surgimiento de una alianza entre la clase media y la burguesia limeña
+    C- La instauracion de un modelo de desarrollo bajo la hegemonia de la mineria
